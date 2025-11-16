e5510e21cc6e2327ecf0e4eddaf8180e05d58d00
KK <music89.films@gmail.com>
2025-11-16 18:03:11 +0100

    primo commit

---
+++ src/main/java/com/tombola/tombola_service/TombolaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tombola.tombola_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TombolaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TombolaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tombola/tombola_service/TombolaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tombola.tombola_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TombolaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
